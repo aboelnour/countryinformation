223b6c0cf332d7dc8c80254000b00cf623bd4036
Mohamed Aboelnour <aboelnour7@gmail.com>
2019-11-02 00:31:10 +0200

    Init Commit

---
+++ src/main/java/com/flairstech/countryinformation/CountryinformationApplication.java
@@ -0,0 +1,13 @@
+package com.flairstech.countryinformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryinformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryinformationApplication.class, args);
+	}
+
+}
